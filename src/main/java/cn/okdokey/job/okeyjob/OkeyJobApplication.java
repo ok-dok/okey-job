bb35bd8ffdb230bc0ebc8a084a5bf76dd7a58ba6
Shawn <shawn_hou@163.com>
2019-01-17 13:42:38 +0800

    spring-boot项目创建

---
+++ src/main/java/cn/okdokey/job/okeyjob/OkeyJobApplication.java
@@ -0,0 +1,14 @@
+package cn.okdokey.job.okeyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkeyJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkeyJobApplication.class, args);
+    }
+
+}
+
